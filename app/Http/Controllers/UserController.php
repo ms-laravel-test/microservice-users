7cac568adec9cc01703d272e7fa732a981ac9157
Ali Kazemayni <116560378+aliKazemayni@users.noreply.github.com>
2025-03-11 17:40:43 +0330

    feat(api): install api and create user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): \Illuminate\Http\JsonResponse
+    {
+        $users = User::all();
+        foreach ($users as $user){
+            $user->note = Http::get("http://microservice-note:8003/api/user/{$user->id}/notes")->json();
+        }
+        return response()->json($users);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
